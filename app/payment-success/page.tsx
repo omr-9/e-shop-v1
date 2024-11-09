884d7d1a932aeb750850be9c1ca1fbfa5e56996a
omr-9 <omaralsharkawi9@gmail.com>
2024-11-09 18:03:13 +0200

    update

--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -1,26 +1,34 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation"; // Client-side useSearchParams hook
 
-export default function PaymentSuccess({
-    searchParams: { totalPrice },
-  }: {
-    searchParams: { totalPrice: string };
-  }) {
-    return (
-      <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-indigo-950">
-        <div className="mb-10">
-          <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-          <h2 className="text-2xl">You successfully sent</h2>
-  
-          <div className="bg-white p-2 rounded-md text-indigo-950 mt-5 text-4xl font-bold">
-            $ {totalPrice}
-          </div>
-        
+export default async function PaymentSuccess({
+  searchParams,
+}: {
+  searchParams: { totalPrice: string }; // Defining type for searchParams
+}) {
+  // Destructuring to get the `totalPrice` from searchParams
+  const { totalPrice } = await searchParams;
 
-        <Link href='/'>
-        <Button className="text-xl hover:bg-indigo-950 hover:border  bg-white mt-6 text-indigo-950 font-semibold hover:text-white " size={'lg'}>Buy More </Button>
-        </Link>
+  return (
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-indigo-950">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
+        <h2 className="text-2xl">You successfully sent</h2>
+
+        <div className="bg-white p-2 rounded-md text-indigo-950 mt-5 text-4xl font-bold">
+          $ {totalPrice}
         </div>
-      </main>
-    );
-  }
+
+        <Link href="/">
+          <Button
+            className="text-xl hover:bg-indigo-950 hover:border  bg-white mt-6 text-indigo-950 font-semibold hover:text-white "
+            size={"lg"}
+          >
+            Buy More{" "}
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
